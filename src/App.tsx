542f0869511441f0104176de458153faca2d0f43
RadoslawSmoronski <radoslaw.smo@gmail.com>
2026-03-21 16:21:47 +0100

    feat: project adding view (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import { ProjectProvider } from "./context/ProjectContext"
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import AddProjectPage from "./pages/AddProjectPage"
+
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects/new" element={<AddProjectPage />} />
         </Routes>
       </div>
     </ProjectProvider>
